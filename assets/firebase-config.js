2577f3b0e0c47ceb4527ea5fe3b3e62c3201ed85
omartameradel <omartameradel600@gmail.com>
2026-09-06 02:04:38 +0300

    Update firebase-config.js

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,15 +1,1 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAA-iRIXpM_xkN3btGyr7elW8mwiuDbTLo",
-  authDomain: "wallet-tamer-adel.firebaseapp.com",
-  projectId: "wallet-tamer-adel",
-  storageBucket: "wallet-tamer-adel.firebasestorage.app",
-  messagingSenderId: "51007948250",
-  appId: "1:51007948250:web:e1f6eaaebb4202b8aface3",
-  measurementId: "G-M9LTMX3LHV"
-};
-
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+import{initializeApp}from'https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js';import{getFirestore}from'https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js';const firebaseConfig={'apiKey':'AIzaSyAA-iRIXpM_xkN3btGyr7elW8mwiuDbTLo','authDomain':'wallet-tamer-adel.firebaseapp.com','projectId':'wallet-tamer-adel','storageBucket':'wallet-tamer-adel.firebasestorage.app','messagingSenderId':'51007948250','appId':'1:51007948250:web:e1f6eaaebb4202b8aface3','measurementId':'G-M9LTMX3LHV'};globalThis['firebaseConfig']=firebaseConfig;export const app=initializeApp(firebaseConfig);export const db=getFirestore(app);
